0c59886ab29e4ee507acbba9c8243a245006bdc4
ChenLongMaster <38415163+ChenLongMaster@users.noreply.github.com>
2021-04-07 23:04:24 +0700

    add program

---
+++ InterveiwPreview/InterveiwPreview/Program.cs
@@ -0,0 +1,51 @@
+﻿using InterveiwPreview.Common;
+using InterveiwPreview.Functions;
+using InterveiwPreview.Functions.Algorithm;
+using System;
+using System.Collections;
+
+namespace InterveiwPreview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.ReadLine();
+        }
+        //static void Main(string[] args)
+        //{
+        //    //
+        //    int[] arr = { 1, 2, 2, 1 };
+        //    MonotonicArray monotonicArray = new MonotonicArray();
+        //    var result1 = monotonicArray.IsMonotonic(arr);
+        //    //
+        //    int[] listNodeArray = { 3, 2, 0, -4 };
+        //    var listNodeRoot = NodeBuilder.NodeBuilderReverse(listNodeArray,1);
+        //    LinkedListCycle linkedListCycle = new LinkedListCycle();
+        //    var result2 = linkedListCycle.HasCycle(listNodeRoot);
+
+        //    //
+        //    int[] SortArray = { 51,95,66,72,42,38,39,41,15};
+
+        //    BubbleSort bubble = new BubbleSort();
+        //    QuickSort quick = new QuickSort();
+        //    //var sortResult1 = bubble.sort(SortArray);
+        //    var sortResult2 = quick.sort(SortArray,0,SortArray.Length - 1);
+
+        //    for (int i = 0; i < sortResult2.Length; i ++)
+        //    {
+        //        Console.WriteLine(sortResult2[i]);
+        //    }
+
+        //    //
+        //    int[] TreeArray = { 51, 95, 66, 72, 42, 38, 39, 41, 15 };
+
+        //    BinaryTree binaryTree = new BinaryTree();
+        //    binaryTree.InsertTreeFromList(TreeArray);
+        //    var a = binaryTree.Search(66);
+        //    Console.WriteLine(a);
+        //    Console.ReadLine();
+        //}
+    }
+}
